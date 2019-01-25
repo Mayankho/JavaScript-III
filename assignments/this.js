e1d740ccf79a9a5d71ec37b1dc6c240807ee5d66
Mayankho <yankhot96@gmail.com>
2019-01-24 23:18:04 -0500

    prototype refractor

--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,14 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global scope - When "this" is not pointing to anything created or rests in a fuction that is pointed in the global scope it will console log whatever is the browsers window or object.
+* 2. Implicit Binding - Most common use of this, refers to when this is when you call this inside of an object or inside of a constructior that takes in an object as the argument.  
+* 3. New binding - This  is when this is activated by the new keyword, when you type the new keyword it searches for a matching constructor function for the word that follows right after it, inside 
+     - the constructor function all the this keywords are activated when the object is created,
+* 4. Explicit binding - 
 *
 * write out a code example of each explanation above
-*/
+
 
 // Principle 1
 
@@ -23,4 +24,4 @@
 
 // Principle 4
 
-// code example for Explicit Binding
+// code example for Explicit Binding */
